8b382acb8128351663f1ac801bf6a1a5caa71751
vaishnavms9656 <vaishnavms9656@gmail.com>
2024-03-14 13:34:20 +0530

    news

--- services/db.js
+++ services/db.js
@@ -1,20 +1,26 @@
-//  Connection code of node and mongo db
+// Import dotenv to load environment variables from .env file
+require('dotenv').config();
 
-//1 Import mongoose
-const mongoose=require('mongoose')
-DATABASE='mongodb://localhost:27017/contact'
+// Import mongoose
+const mongoose = require('mongoose');
 
-//2 Connection string
-mongoose.connect(DATABASE)
+// Get the database connection string from environment variables
+const DATABASE = process.env.DATABASE;
 
-//3 Model Creation
-const contact=mongoose.model('contacts',{
-    id:Number,
-    name:{firstname:String,lastname:String},
-    email:String,
-    phone:String,
-    address:{city:String,street:String,zipcode:String},
-})
-module.exports={
+// Connection string
+mongoose.connect(DATABASE);
+
+// Model Creation
+const contactSchema = new mongoose.Schema({
+    id: Number,
+    name: { firstname: String, lastname: String },
+    email: String,
+    phone: String,
+    address: { city: String, street: String, zipcode: String },
+});
+
+const contact = mongoose.model('contacts', contactSchema);
+
+module.exports = {
     contact
-}
+};
